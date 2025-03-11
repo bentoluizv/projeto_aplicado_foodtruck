a833ec12eff33a75fc18b8122da2415388505226
bentoluizv <bentoluizv@gmail.com>
2025-03-11 11:19:21 -0300

    chore: collecting logs and tracers in fastapi app

--- projeto_aplicado/app.py
+++ projeto_aplicado/app.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(
     debug=True,
@@ -7,7 +14,16 @@ app = FastAPI(
     description='API para o projeto aplicado do SENAI 2025',
 )
 
+provider = TracerProvider()
+exporter = OTLPSpanExporter(endpoint='http://grafana:3000')
+processor = BatchSpanProcessor(exporter)
+provider.add_span_processor(processor)
+
 
 @app.get('/')
 async def read_root():
     return {'message': 'Hello World'}
+
+
+Instrumentator().instrument(app).expose(app)
+FastAPIInstrumentor.instrument_app(app, tracer_provider=provider)
