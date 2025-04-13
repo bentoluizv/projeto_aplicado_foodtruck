5c2f5387ea6135fd44d6a736c0adf7f07a23d16e
bentoluizv <bentoluizv@gmail.com>
2025-04-13 11:56:47 -0300

    add utils func

---
+++ projeto_aplicado/utils.py
@@ -0,0 +1,23 @@
+from ulid import ULID
+
+from projeto_aplicado.settings import Settings
+
+
+def get_ulid_as_str():
+    """
+    Retorna um ULID único.
+
+    :return: str.
+    """
+
+    return str(ULID())
+
+
+def get_db_url(settings: Settings):
+    """
+    Retorna a URL de conexão com o banco de dados.
+
+    :return: str.
+    """
+
+    return f'postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}'
