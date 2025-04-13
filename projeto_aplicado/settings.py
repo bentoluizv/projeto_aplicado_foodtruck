832aae1281227dd64fd4fb8174f877485e664c21
bentoluizv <bentoluizv@gmail.com>
2025-04-13 11:57:00 -0300

    chore: add project settings

---
+++ projeto_aplicado/settings.py
@@ -0,0 +1,35 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+    )
+    # Database
+    DB_ECHO: bool
+    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str
+    POSTGRES_DB: str
+    POSTGRES_HOSTNAME: str
+    POSTGRES_PORT: str
+    # Redis
+    REDIS_URL: str
+    # Fastapi
+    API_DEBUG: bool
+    API_VERSION: str
+    API_PREFIX: str
+    # S3
+    S3_ACCESS_KEY_ID: str
+    S3_ACCESS_KEY_SECRET: str
+    S3_ENDPOINT: str
+    S3_REGION: str
+
+
+def get_settings() -> Settings:
+    """
+    Retorna as configurações do projeto.
+
+    :return: Configurações do projeto.
+    """
+    return Settings()  # type: ignore
