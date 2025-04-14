0309fedf0675644b7a05ffda58a1cb2f6905eba7
bentoluizv <bentoluizv@gmail.com>
2025-04-14 08:47:25 -0300

    feat: refactor app.py to include get_engine and adjust lifespan handling

--- projeto_aplicado/app.py
+++ projeto_aplicado/app.py
@@ -4,17 +4,17 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from projeto_aplicado import controllers, pages
-from projeto_aplicado.ext.database.db import create_all
+from projeto_aplicado.ext.database.db import create_all, get_engine
 from projeto_aplicado.settings import get_settings
 
 settings = get_settings()
+
 
 app = FastAPI(
     debug=settings.API_DEBUG,
     title='Projeto Aplicado SENAI 2025',
     version=settings.API_VERSION,
     description='API para o projeto aplicado do SENAI 2025',
-    lifespan=create_all(),
 )
 
 app.mount(
@@ -25,3 +25,8 @@ app.mount(
 
 app.include_router(pages.router)
 app.include_router(controllers.router)
+
+
+if __name__ == '__main__':
+    engine = get_engine()
+    create_all(engine)
