c23a8617ab67aae1c569528d4587a9c76f34bbc7
bentoluizv <bentoluizv@gmail.com>
2025-04-14 08:51:23 -0300

    feat: enhance test fixtures in conftest.py for improved session management

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,38 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlmodel import Session, StaticPool, create_engine
 
 from projeto_aplicado.app import app
+from projeto_aplicado.ext.database.db import create_all, drop_all, get_session
+
+
+@pytest.fixture(scope='session')
+def engine():
+    engine = create_engine(
+        'sqlite+pysqlite://',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
+    return engine
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def session(engine):
+    create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    drop_all(engine)
+
+
+@pytest.fixture
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
