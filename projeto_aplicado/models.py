0c136b7ec42af874c428b094e7e5c32c824d6516
bentoluizv <bentoluizv@gmail.com>
2025-04-13 21:10:48 -0300

    fix: correct id and category_id field types in models

--- projeto_aplicado/models.py
+++ projeto_aplicado/models.py
@@ -4,21 +4,17 @@ from projeto_aplicado.utils import get_ulid_as_str
 
 
 class Category(SQLModel, table=True):
-    id: str | None = Field(
-        default=None, default_factory=get_ulid_as_str, primary_key=True
-    )
+    id: str = Field(default_factory=get_ulid_as_str, primary_key=True)
     name: str
     description: str | None = None
     itens: list['Item'] = Relationship(back_populates='category')
 
 
 class Item(SQLModel, table=True):
-    id: str | None = Field(
-        default=None, default_factory=get_ulid_as_str, primary_key=True
-    )
+    id: str = Field(default_factory=get_ulid_as_str, primary_key=True)
     name: str = Field(max_length=80, index=True)
     description: str | None = Field(default=None, max_length=255)
     price: float = Field(nullable=False, gt=0.0)
     image: bytes | None = Field(default=None)
-    category_id: int | None = Field(default=None, foreign_key='category.id')
+    category_id: str = Field(foreign_key='category.id')
     category: Category = Relationship(back_populates='itens')
