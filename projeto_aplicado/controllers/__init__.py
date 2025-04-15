c92a202788733dd9f5d83d26529465bcb607a78e
bentoluizv <bentoluizv@gmail.com>
2025-04-15 00:37:01 -0300

    feat: add Jinja2Templates configuration and implement template rendering in controllers

--- projeto_aplicado/controllers/__init__.py
+++ projeto_aplicado/controllers/__init__.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 
 from projeto_aplicado.controllers import category_controller, item_controller
 from projeto_aplicado.settings import get_settings
@@ -6,6 +7,23 @@ from projeto_aplicado.settings import get_settings
 settings = get_settings()
 
 router = APIRouter(prefix=settings.API_PREFIX)
+
+templates = Jinja2Templates(
+    directory='templates',
+    auto_reload=True,
+    cache_size=0,
+)
+
+
+@router.get('/')
+async def home_page(request: Request):
+    """
+    Serve a página principal do projeto, retorna um HTML Response.
+
+    """
+
+    return templates.TemplateResponse('index.html', {'request': request})
+
 
 router.include_router(item_controller.router)
 router.include_router(category_controller.router)
--- projeto_aplicado/controllers/category_controller.py
+++ projeto_aplicado/controllers/category_controller.py
@@ -1,8 +1,10 @@
 from http import HTTPStatus
-from typing import Annotated
+from typing import Annotated, Union
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, Header, HTTPException, Query, Request
+from fastapi.templating import Jinja2Templates
 
+# from projeto_aplicado.ext.cache.redis import get_many
 from projeto_aplicado.models.entities import Category
 from projeto_aplicado.models.schemas import (
     BaseResponse,
@@ -14,6 +16,13 @@ from projeto_aplicado.repositories.category_repository import (
     get_category_repository,
 )
 
+templates = Jinja2Templates(
+    directory='templates',
+    auto_reload=True,
+    cache_size=0,
+)
+
+
 router = APIRouter(tags=['Category'], prefix='/categories')
 
 CategoryRepo = Annotated[CategoryRepository, Depends(get_category_repository)]
@@ -21,7 +30,9 @@ CategoryRepo = Annotated[CategoryRepository, Depends(get_category_repository)]
 
 @router.get('/', response_model=list[Category])
 def get_categories(
+    request: Request,
     repository: CategoryRepo,
+    hx_request: Annotated[Union[str, None], Header()] = None,
     offset: int = 0,
     limit: int = Query(default=100, le=100),
 ):
@@ -29,7 +40,19 @@ def get_categories(
     Get all categories.
 
     """
+
+    # cache = get_many('categories')
+
+    # if cache:
+    #     return cache
+
     categories = repository.get_all(offset=offset, limit=limit)
+
+    if hx_request:
+        return templates.TemplateResponse(
+            request, 'categories.html', context={'categories': categories}
+        )
+
     return categories
 
 
