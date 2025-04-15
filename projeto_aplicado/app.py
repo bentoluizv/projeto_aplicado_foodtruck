00380c72e65b06495036d92a87feb14cf10d487f
bentoluizv <bentoluizv@gmail.com>
2025-04-15 09:12:51 -0300

    refactor: reorganize imports and clean up unused code in app.py

--- projeto_aplicado/app.py
+++ projeto_aplicado/app.py
@@ -4,17 +4,26 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from projeto_aplicado import controllers, pages
-from projeto_aplicado.ext.database.db import create_all, get_engine
+from projeto_aplicado.ext.database.db import (  # noqa: F401
+    create_all,
+    get_engine,
+)
 from projeto_aplicado.settings import get_settings
 
 settings = get_settings()
 
+engine = get_engine()
 
 app = FastAPI(
     debug=settings.API_DEBUG,
     title='Projeto Aplicado SENAI 2025',
     version=settings.API_VERSION,
     description='API para o projeto aplicado do SENAI 2025',
+    # # Se deixar o lifespan, não é possível rodar os testes com pytest,
+    # pois ele tenta executar o lifespan antes da fixture que faz override
+    # do engine. Isso gera um erro, pois o engine não está configurado para
+    # o banco de dados correto. Ainda tentando descobrir como resolver isso.
+    # lifespan=create_all(engine),
 )
 
 app.mount(
@@ -25,8 +34,3 @@ app.mount(
 
 app.include_router(pages.router)
 app.include_router(controllers.router)
-
-
-if __name__ == '__main__':
-    engine = get_engine()
-    create_all(engine)
