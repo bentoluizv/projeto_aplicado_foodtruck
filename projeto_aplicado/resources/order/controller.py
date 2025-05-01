3e5b81fb4107865e5f1edf10893cf5725917566d
bentoluizv <bentoluizv@gmail.com>
2025-05-01 17:47:15 -0300

    feat: implement Order controller with CRUD operations and pagination support

--- projeto_aplicado/resources/order/controller.py
+++ projeto_aplicado/resources/order/controller.py
@@ -16,15 +16,13 @@ from projeto_aplicado.resources.order.repository import (
     OrderRepository,
     get_order_repository,
 )
+from projeto_aplicado.resources.order.schemas import OrderList, UpdateOrderDTO
 from projeto_aplicado.resources.order_item.model import OrderItem
 from projeto_aplicado.resources.product.repository import (
     ProductRepository,
     get_product_repository,
 )
-from projeto_aplicado.schemas import (
-    BaseResponse,
-    UpdateProductDTO,
-)
+from projeto_aplicado.schemas import BaseResponse
 from projeto_aplicado.settings import get_settings
 
 templates = Jinja2Templates(
@@ -40,11 +38,18 @@ ProductRepo = Annotated[ProductRepository, Depends(get_product_repository)]
 router = APIRouter(tags=['Order'], prefix=f'{settings.API_PREFIX}/orders')
 
 
-@router.get('/', response_model=list[Order])
+@router.get('/', response_model=OrderList, status_code=HTTPStatus.OK)
 def get_orders(repository: OrderRepo, offset: int = 0, limit: int = 100):
     """
     Get all orders.
+    Args:
+        repository (OrderRepo): The order repository.
+        offset (int): The offset for pagination.
+        limit (int): The maximum number of orders to retrieve.
+    Returns:
+        OrderList: A list of orders with pagination information.
     """
+
     orders = repository.get_all(offset=offset, limit=limit)
 
     return orders
@@ -54,6 +59,13 @@ def get_orders(repository: OrderRepo, offset: int = 0, limit: int = 100):
 def get_product_by_id(order_id: str, repository: OrderRepo):
     """
     Get a order by ID.
+    Args:
+        order_id (str): The ID of the order to retrieve.
+        repository (OrderRepo): The order repository.
+    Returns:
+        Order: The order with the specified ID.
+    Raises:
+        HTTPException: If the order with the specified ID is not found.
     """
     order = repository.get_by_id(order_id)
 
@@ -78,6 +90,18 @@ async def create_order(  # noqa: PLR0913, PLR0917
 ):
     """
     Create a new order.
+    Args:
+        request (Request): The HTTP request object.
+        order_itens (list[dict]): A list of order items.
+        customer_id (str): The ID of the customer.
+        notes (str): Additional notes for the order.
+        order_repository (OrderRepo): The order repository.
+        product_repository (ProductRepo): The product repository.
+        hx_request (Union[str, None]): Optional header for HTMX requests.
+    Returns:
+        BaseResponse: A response indicating the result of the operation.
+    Raises:
+        HTTPException: If a product in the order items is not found.
     """
 
     new_order = Order(
@@ -97,7 +121,7 @@ async def create_order(  # noqa: PLR0913, PLR0917
         order_item = OrderItem(
             quantity=item['quantity'],
             price=product.price,
-            order_id='',
+            order_id=new_order.id,
             product_id=item['product_id'],
         )
 
@@ -105,68 +129,69 @@ async def create_order(  # noqa: PLR0913, PLR0917
 
     order_repository.create(new_order)
 
-    # if hx_request:
-    #     return templates.TemplateResponse(
-    #         request,
-    #         'selected_category_new_products.html',
-    #         headers={'HX-Trigger': 'productsUpdated'},
-    #         context={
-    #             'product': order_repository.get_all(),
-    #         },
-    #         status_code=HTTPStatus.CREATED,
-    #     )
+    if hx_request:
+        pass
 
     return BaseResponse(id=new_order.id, action='created')
 
 
-@router.patch('/{product_id}', response_model=BaseResponse)
-def update_product(
-    product_id: str,
-    dto: UpdateProductDTO,
+@router.patch('/{order_id}', response_model=BaseResponse)
+def update_order(
+    order_id: str,
+    dto: UpdateOrderDTO,
     repository: OrderRepo,
 ):
     """
-    Update an product by ID.
+    Update an order by ID.
+    Returns:
+        BaseResponse: A response indicating the result of the operation.
+    Raises:
+        HTTPException: If the order with the specified ID is not found.
     """
 
-    existing_product = repository.get_by_id(product_id)
-    if not existing_product:
+    existing_order = repository.get_by_id(order_id)
+    if not existing_order:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
-            detail='product not found',
+            detail='Order not found',
         )
-    repository.update(existing_product, dto)
-    return BaseResponse(id=existing_product.id, action='updated')
+
+    repository.update(existing_order, dto)
+
+    return BaseResponse(id=existing_order.id, action='updated')
 
 
-@router.delete('/{product_id}', response_model=BaseResponse)
-def delete_product(
+@router.delete('/{order_id}', response_model=BaseResponse)
+def delete_order(
     request: Request,
-    product_id: str,
+    order_id: str,
     repository: OrderRepo,
     hx_request: Annotated[Union[str, None], Header()] = None,
 ):
     """
-    Delete an product by ID.
+    Delete an order by ID.
+    Args:
+        request (Request): The HTTP request object.
+        order_id (str): The ID of the order to delete.
+        repository (OrderRepo): The order repository.
+        hx_request (Union[str, None]): Optional header for HTMX requests.
+    Returns:
+        BaseResponse: A response indicating the result of the operation.
+    Raises:
+        HTTPException: If the order with the specified ID is not found.
     """
 
-    existing_product = repository.get_by_id(product_id)
+    existing_order = repository.get_by_id(order_id)
 
-    if not existing_product:
+    if not existing_order:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
-            detail='product not found',
+            detail='Order not found',
         )
 
-    repository.delete(existing_product)
+    repository.delete(existing_order)
 
     if hx_request:
-        return templates.TemplateResponse(
-            request,
-            'selected_category_new_products.html',
-            headers={'HX-Trigger': 'productsUpdated'},
-            context={'categories': repository.get_all()},
-            status_code=HTTPStatus.OK,
-        )
+        pass
 
-    return BaseResponse(id=existing_product.id, action='deleted')
+    return BaseResponse(id=existing_order.id, action='deleted')
