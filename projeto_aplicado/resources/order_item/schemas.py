ef2d932a17165db3da7cdd3766f8bc21c7c16369
bentoluizv <bentoluizv@gmail.com>
2025-05-01 17:46:31 -0300

    feat: add schemas for order item creation, update, and listing with pagination support

--- projeto_aplicado/resources/order_item/schemas.py
+++ projeto_aplicado/resources/order_item/schemas.py
@@ -1,4 +1,9 @@
+from typing import Sequence
+
 from sqlmodel import SQLModel
+
+from projeto_aplicado.resources.order_item.model import OrderItem
+from projeto_aplicado.schemas import Pagination
 
 
 class CreateOrderItemDTO(SQLModel):
@@ -6,3 +11,13 @@ class CreateOrderItemDTO(SQLModel):
     price: float
     order_id: str
     product_id: str
+
+
+class UpdateOrderItemDTO(SQLModel):
+    quantity: int | None = None
+    price: float | None = None
+
+
+class OrderItemList(SQLModel):
+    order_itens: Sequence[OrderItem]
+    pagination: Pagination
