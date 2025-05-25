67964d7601343b6a89aa0d6bbcbe0be0beb5f980
bentoluizv <bentoluizv@gmail.com>
2025-05-25 15:00:53 -0300

    feat: integrate current user dependency and include token router in FastAPI application

--- projeto_aplicado/app.py
+++ projeto_aplicado/app.py
@@ -3,6 +3,8 @@ from typing import Annotated
 from fastapi import Depends, FastAPI
 from supabase import Client
 
+from projeto_aplicado.auth.security import get_current_user
+from projeto_aplicado.auth.token import router as token_router
 from projeto_aplicado.ext.database.db import get_engine
 from projeto_aplicado.ext.supabase.client import get_supabase_client
 from projeto_aplicado.ext.supabase.storage import list_all_icons
@@ -10,12 +12,14 @@ from projeto_aplicado.resources.order.controller import router as order_router
 from projeto_aplicado.resources.product.controller import router as item_router
 from projeto_aplicado.resources.shared.schemas import IconsResponse
 from projeto_aplicado.resources.users.controller import router as user_router
+from projeto_aplicado.resources.users.model import User
 from projeto_aplicado.settings import get_settings
 
 settings = get_settings()
 engine = get_engine()
 
 Supabase = Annotated[Client, Depends(get_supabase_client)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
 
 app = FastAPI(
     debug=settings.API_DEBUG,
@@ -38,7 +42,7 @@ async def home():
 
 
 @app.get('/icons', response_model=IconsResponse)
-async def get_icons(supabase: Supabase):
+async def get_icons(supabase: Supabase, current_user: CurrentUser):
     """
     Returns a list of available icons.
     """
@@ -49,3 +53,4 @@ async def get_icons(supabase: Supabase):
 app.include_router(item_router)
 app.include_router(order_router)
 app.include_router(user_router)
+app.include_router(token_router)
