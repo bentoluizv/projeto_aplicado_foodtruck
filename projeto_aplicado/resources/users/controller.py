e16896a188e0b9b959bfb27262755327e8262e60
bentoluizv <bentoluizv@gmail.com>
2025-05-25 00:07:25 -0300

    feat: implement user controller with CRUD operations for user management, including fetching, creating, updating, and deleting users

---
+++ projeto_aplicado/resources/users/controller.py
@@ -0,0 +1,107 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+
+from projeto_aplicado.resources.shared.schemas import Pagination
+from projeto_aplicado.resources.users.model import User
+from projeto_aplicado.resources.users.repository import (
+    UserRepository,
+    get_user_repository,
+)
+from projeto_aplicado.resources.users.schemas import (
+    CreateUserDTO,
+    UpdateUserDTO,
+    UserList,
+    UserOut,
+)
+from projeto_aplicado.settings import get_settings
+
+settings = get_settings()
+UserRepo = Annotated[UserRepository, Depends(get_user_repository)]
+router = APIRouter(tags=['User'], prefix=f'{settings.API_PREFIX}/users')
+
+
+@router.get('/', response_model=UserList, status_code=HTTPStatus.OK)
+def fetch_users(repository: UserRepo, offset: int = 0, limit: int = 100):
+    users = repository.get_all(offset=offset, limit=limit)
+    return UserList(
+        items=[  # type: ignore
+            UserOut(
+                id=user.id,
+                name=user.name,
+                email=user.email,
+                role=user.role,
+                created_at=user.created_at,
+                updated_at=user.updated_at,
+            )
+            for user in users
+        ],
+        pagination=Pagination(
+            offset=offset,
+            limit=limit,
+            total_count=len(users),
+            total_pages=len(users) // limit if limit > 0 else 0,
+            page=offset // limit + 1 if limit > 0 else 1,
+        ),
+    )
+
+
+@router.get('/{user_id}', response_model=UserOut)
+def fetch_user_by_id(user_id: str, repository: UserRepo):
+    user = repository.get_by_id(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    return UserOut(
+        id=user.id,
+        name=user.name,
+        email=user.email,
+        role=user.role,
+        created_at=user.created_at,
+        updated_at=user.updated_at,
+    )
+
+
+@router.post('/', response_model=UserOut, status_code=HTTPStatus.CREATED)
+def create_user(dto: CreateUserDTO, repository: UserRepo):
+    user = User(**dto.model_dump())
+    repository.create(user)
+    return UserOut(
+        id=user.id,
+        name=user.name,
+        email=user.email,
+        role=user.role,
+        created_at=user.created_at,
+        updated_at=user.updated_at,
+    )
+
+
+@router.patch('/{user_id}', response_model=UserOut)
+def update_user(user_id: str, dto: UpdateUserDTO, repository: UserRepo):
+    user = repository.get_by_id(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    repository.update(user, dto)
+    return UserOut(
+        id=user.id,
+        name=user.name,
+        email=user.email,
+        role=user.role,
+        created_at=user.created_at,
+        updated_at=user.updated_at,
+    )
+
+
+@router.delete('/{user_id}', status_code=HTTPStatus.OK)
+def delete_user(user_id: str, repository: UserRepo):
+    user = repository.get_by_id(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    repository.delete(user)
+    return {'action': 'deleted', 'id': user_id}
