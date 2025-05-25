ccf5074bdecb914988934b5a5ff5f82e61f8de9d
bentoluizv <bentoluizv@gmail.com>
2025-05-25 14:59:56 -0300

    feat: add category field to Product model and enforce unique constraint on product name

--- projeto_aplicado/resources/product/model.py
+++ projeto_aplicado/resources/product/model.py
@@ -1,5 +1,6 @@
 from sqlmodel import Field
 
+from projeto_aplicado.resources.product.enums import ProductCategory
 from projeto_aplicado.resources.shared.model import BaseModel
 
 
@@ -8,11 +9,12 @@ class Product(BaseModel, table=True):
     Product model representing a product in the system.
     """
 
-    name: str = Field(max_length=80, index=True, nullable=False)
+    name: str = Field(max_length=80, index=True, nullable=False, unique=True)
     description: str | None = Field(
         default=None, max_length=255, nullable=True
     )
     price: float = Field(nullable=False, gt=0.0)
+    category: ProductCategory = Field(nullable=False)
 
     @classmethod
     def create(cls, dto: 'CreateProductDTO'):  # type: ignore  # noqa: F821
